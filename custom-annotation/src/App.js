fcdfcb0455e64a4a5e5a9430afd120403ba94c04
adityasuman2025 <adityasuman2025@gmail.com>
2022-12-08 19:33:31 +0530

    draw annot done

--- custom-annotation/src/App.js
+++ custom-annotation/src/App.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { ResizeProvider, ResizeConsumer } from "react-resize-context"; //ref: https://codesandbox.io/embed/jjjmp4z6yy
 
 import img from "./amrit.jpg";
 import tickImg from "./tick.png";
 import crossImg from "./cross.png";
-
 import deleteImg from "./delete.png";
 import rotateImg from "./rotate.png";
 
@@ -25,16 +24,21 @@ function App() {
     const [annot, setAnnot] = useState([]);
     const [selectedAnnot, setSelectedAnnot] = useState("");
 
+    const [drawing, setDrawing] = useState(false);
+    const drawRef = useRef(null);
+    const ctxRef = useRef(null);
+
     useEffect(() => {
+        //retriving annotations from localStorage
+        const annotStorage = JSON.parse(localStorage.getItem("annot") || "[]");
+        setAnnot(annotStorage);
+
         const imgEle = document.createElement("img");
         imgEle.src = img;
         imgEle.onload = function () {
             setImgDim({ height: imgEle.height / 2.2, width: imgEle.width / 1.5 });
+            renderDraw({ height: imgEle.height / 2.2, width: imgEle.width / 1.5 }, annotStorage);
         }
-
-        //retriving annotations from localStorage
-        const annotStorage = localStorage.getItem("annot") || "[]";
-        setAnnot(JSON.parse(annotStorage));
     }, []);
 
     useEffect(() => {
@@ -51,6 +55,8 @@ function App() {
     }
 
     function handleCanvasClick() {
+        if (selectedBtn === "pencil") return;
+
         if (selectedBtn) setAnnot([...annot, { src: selectedBtn, pos: { x: mousePos.x, y: mousePos.y } }]);
     }
 
@@ -58,7 +64,6 @@ function App() {
         e.stopPropagation();
 
         setSelectedAnnot(idx);
-        console.log("handleAnnotClick", idx)
     }
 
     function handleAnotDeleteClick(e, idx) {
@@ -121,22 +126,105 @@ function App() {
         setTimeout(() => e.target.classList.remove("invisibleAnnot"), 0);
     }
 
+    function renderDraw({ height, width }, annot) {
+        //ref: https://blog.openreplay.com/2d-sketches-with-react-and-the-canvas-api
+
+        const canvas = drawRef.current;
+
+        // For supporting computers with higher screen densities, we double the screen density
+        canvas.width = width;
+        canvas.height = height;
+        canvas.style.width = `${width}px`;
+        canvas.style.height = `${height}px`;
+
+        // Setting the context to enable us draw
+        const ctx = canvas.getContext('2d');
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 8;
+        ctxRef.current = ctx;
+
+        // loading older image/canvas data in canvas
+        renderImagesInDraw(annot);
+    }
+
+    function renderImagesInDraw(annot) {
+        if (!ctxRef.current || !drawRef.current) return;
+
+        ctxRef.current.clearRect(0, 0, drawRef.current.width, drawRef.current.height); //clearing old image/canvas data
+
+        for (let i = 0; i < annot.length; i++) {
+            let { type, src } = annot[i] || {};
+
+            if (type == "draw") {
+                let imgEle = new Image();
+                imgEle.src = src;
+                imgEle.onload = function () {
+                    ctxRef.current.drawImage(imgEle, 0, 0);
+                };
+            }
+        }
+    }
+
+    function startDraw({ nativeEvent }) {
+        const { offsetX, offsetY } = nativeEvent;
+        ctxRef.current.beginPath();
+        ctxRef.current.moveTo(offsetX, offsetY);
+
+        setDrawing(true);
+    }
+
+    function stopDraw() {
+        ctxRef.current.closePath();
+        setDrawing(false);
+
+        // converting the draw into image and storing in annot state
+        const canvas = drawRef.current;
+        const imgData = canvas.toDataURL("image/png");
+        // const imgData = ctx.getImageData(0, 0, imgDim.width, imgDim.height);
+
+        setAnnot([...annot, { src: imgData, type: "draw" }]);
+    }
+
+    function draw({ nativeEvent }) {
+        if (!drawing) return;
+        const { offsetX, offsetY } = nativeEvent;
+        ctxRef.current.lineTo(offsetX, offsetY);
+        ctxRef.current.stroke();
+    }
+
+    function handleUndoClick() {
+        const updatedAnnot = annot.slice(0, -1)
+        setAnnot(updatedAnnot);
+
+        renderImagesInDraw(updatedAnnot); //if any canvas image is undoed then it will be removed
+    }
+
     return (
         <div onDragOver={(e) => { e.preventDefault() /* to prevent drag shadow going to old position */ }}>
-            <div><button onClick={() => handleBtnClick(tickImg)}><img src={tickImg} /></button></div>
-            <div><button onClick={() => handleBtnClick(crossImg)}><img src={crossImg} /></button></div>
+            <button onClick={() => handleBtnClick(tickImg)}><img src={tickImg} /></button>
+            <button onClick={() => handleBtnClick(crossImg)}><img src={crossImg} /></button>
+            <button onClick={() => handleBtnClick("pencil")}>pencil</button>
+            <button onClick={handleUndoClick}>undo</button>
             <br /><br />
 
-            {
-                imgDim.height ?
-                    <div
-                        id="canvas"
-                        style={{ height: imgDim.height, width: imgDim.width, backgroundImage: `url(${img})` }}
-                        onMouseMove={handleCanvasMouseMove}
-                        onClick={handleCanvasClick}
-                    >
-                        {
-                            annot.map(({ src, pos, size, rotate = 0 }, idx) => (
+            <div
+                id="canvas"
+                style={{ height: imgDim.height, width: imgDim.width, backgroundImage: `url(${img})` }}
+                onMouseMove={handleCanvasMouseMove}
+                onClick={handleCanvasClick}
+            >
+                <canvas
+                    ref={drawRef}
+                    onMouseDown={(e) => { if (selectedBtn === "pencil") startDraw(e) }}
+                    onMouseUp={(e) => { if (selectedBtn === "pencil") stopDraw(e) }}
+                    onMouseMove={(e) => { if (selectedBtn === "pencil") draw(e) }}
+                />
+
+                {
+                    annot.map(({ src, pos, size, rotate = 0, type }, idx) => {
+                        if (type !== "draw")
+                            return (
                                 <div
                                     className={idx == selectedAnnot ? "annot selectedAnnot" : "annot"}
                                     key={idx}
@@ -175,11 +263,10 @@ function App() {
                                         }
                                     </div>
                                 </div>
-                            ))
-                        }
-                    </div>
-                    : "loading"
-            }
+                            )
+                    })
+                }
+            </div>
         </div>
     );
 }
